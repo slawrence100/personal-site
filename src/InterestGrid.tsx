e5043b617908a57388b8fab82b1c743fd87cee43
Sydney Lawrence <sll224@cornell.edu>
2021-11-03 15:11:27 -0400

    Set up gh-pages build
    Add first build
    
    Rename for Github pages

--- src/InterestGrid.tsx
+++ src/InterestGrid.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
 import Card from 'react-bootstrap/Card';
 
 // Images
