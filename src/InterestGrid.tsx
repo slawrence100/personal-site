ec139d8b74ecda8b5697a6e2585a79ded248dc7e
Sydney Lawrence <sll224@cornell.edu>
2021-11-03 14:23:22 -0400

    Add interests images

--- src/InterestGrid.tsx
+++ src/InterestGrid.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+
+// Bootstrap
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Card from 'react-bootstrap/Card';
+
+// Images
+import journal from './journal.jpeg';
+import baking from './baking.jpg';
+import sailing from './sailing.jpg';
+import flute_band from './flute-band.jpg';
+import cooking from './cooking.jpg';
+import mandarin from './mandarin.jpg';
 
 export default function InterestGrid() {
 
@@ -9,27 +21,45 @@ export default function InterestGrid() {
     <Container>
       <Row>
         <Col>
-          <p>Instrument playing</p>
+        <Card>
+            <Card.Img src={flute_band} />
+            <Card.Title>Wind Instruments</Card.Title>
+          </Card>
         </Col>
         <Col>
-          <p>Mandarin Chinese</p>
+        <Card>
+            <Card.Img src={mandarin} />
+            <Card.Title>Mandarin Chinese</Card.Title>
+          </Card>
         </Col>
       </Row>
       <Row>
         <Col>
-          <p>Cooking</p>
+        <Card>
+            <Card.Img src={cooking} />
+            <Card.Title>Cooking</Card.Title>
+          </Card>
         </Col>
         <Col>
-          <p>Sailing?</p>
+        <Card>
+            <Card.Img src={sailing} />
+            <Card.Title>Sailing</Card.Title>
+          </Card>
         </Col>
       </Row>
       <Row>
         <Col>
-          <p>Journaling?</p>
+          <Card>
+            <Card.Img src={journal} />
+            <Card.Title>Journaling</Card.Title>
+          </Card>
         </Col>
         <Col>
-          <p>Baking</p>
-        </Col>
+        <Card>
+            <Card.Img src={baking} />
+            <Card.Title>Baking</Card.Title>
+          </Card>
+          </Col>
       </Row>
     </Container>
   )
